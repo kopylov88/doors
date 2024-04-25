c6ef5858134f8d6acbcf2db9279a9659ef603614
kopylov88 <kopyl.88@mail.ru>
2024-04-25 16:25:27 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,7 @@ myFunctions.isWebp();
 
 if ('ontouchstart' in window || (window.DocumentTouch && document instanceof DocumentTouch)) {
   console.log('this is a touch device');
+  document.body.classList.add('touch');
 } else {
   console.log('this is not a touch device');
   document.body.classList.add('no-touch');
